200aa918ac8a9c0ed3416e6400c93c2aec898e9c
3gbCyber <57197804+3gbCyber@users.noreply.github.com>
2022-05-16 19:12:29 +0300

    Create script.ps1
    V1.0

---
+++ script.ps1
@@ -0,0 +1,9 @@
+$path = $args[0]
+$from = $args[1]
+$to = $args[2]
+$output = $args[3]
+
+Get-ChildItem -Path $path -Force -Recurse -ev AccessErrors -ea silent `
+| Select @{N="Owner";E={ (Get-Acl $_.FullName).Owner }}, Fullname, Length, LastAccessTime, LastWriteTime, CreationTime `
+| ? {$_.lastwritetime -gt $from -AND $_.lastwritetime -lt $to} `
+| Export-Csv -Path  $output -NoTypeInformation
