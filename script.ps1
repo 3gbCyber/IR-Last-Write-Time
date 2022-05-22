a23d16409431d3d59092f5ac39a722a39b194dd6
3gbCyber <57197804+3gbCyber@users.noreply.github.com>
2022-05-22 11:23:26 +0300

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -6,4 +6,4 @@ $output = $args[3]
 Get-ChildItem -Path $path -Force -Recurse -ev AccessErrors -ea silent `
 | Select @{N="Owner";E={ (Get-Acl $_.FullName).Owner }}, Fullname, Length, LastAccessTime, LastWriteTime, CreationTime `
 | ? {$_.lastwritetime -gt $from -AND $_.lastwritetime -lt $to} `
-| Export-Csv -Path  $output -NoTypeInformation
+| Export-Csv -Path  $output -NoTypeInformation -encoding UTF8
