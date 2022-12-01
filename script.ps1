50f10bff59957a8788deebce5d371b2dd97e3269
3gbCyber <57197804+3gbCyber@users.noreply.github.com>
2022-12-02 01:38:07 +0300

    Update script.ps1
    - Added owner
    - Added modify & last access

--- script.ps1
+++ script.ps1
@@ -1,9 +1,10 @@
 $path = $args[0]
-$from = $args[1]
-$to = $args[2]
-$output = $args[3]
+$type = $args[1]
+$from = $args[2]
+$to = $args[3]
+$output = $args[4]
 
 Get-ChildItem -Path $path -Force -Recurse -ev AccessErrors -ea silent `
 | Select @{N="Owner";E={ (Get-Acl $_.FullName).Owner }}, Fullname, Length, LastAccessTime, LastWriteTime, CreationTime `
-| ? {$_.lastwritetime -gt $from -AND $_.lastwritetime -lt $to} `
-| Export-Csv -Path  $output -NoTypeInformation -encoding UTF8
+| ? {$_.$type -gt $from -AND $_.$type -lt $to} `
+| Export-Csv -Path $output -NoTypeInformation
